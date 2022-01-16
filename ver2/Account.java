3b23af46be0e4609d9e7af6b245ca782d5aef4e6
sle321 <97837377+sle321@users.noreply.github.com>
2022-01-16 09:13:58 -0500

    commit title

--- ver2/Account.java
+++ ver2/Account.java
@@ -1,9 +1,9 @@
 package ver2;
 
 public class Account {
+	//New Comment for balance
 	// Instance variable, only available inside this class.
 	private double balance;
-	//New Comment for balance
 	private String name;
 	public Account(double balance) {
 		this("Unknown", balance);
